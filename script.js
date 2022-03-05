ca358efe70a7081ab92de6af8ce79b4892206e84
CDwee <96454637+CDwee@users.noreply.github.com>
2022-03-05 16:45:39 -0600

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Started at 3:26 3-5-2022
+// Tried finding a way to embed my tetris python file into my html code and couldn;t fine anything or anyone that could help me.
+// Ended at 4:44 3-5-2022
